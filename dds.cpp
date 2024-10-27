ead6791eddfb64dc5b43186713ce331220fc2744
Matthew <61363778+matieo33@users.noreply.github.com>
2024-10-27 12:52:45 +0100

    Add files via upload

--- dds.cpp
+++ dds.cpp
@@ -2,13 +2,17 @@
 #include <filesystem>
 #include <chrono>
 
+std::filesystem::path filePath;
+
+const std::filesystem::path inputFolder = "input";
+const std::filesystem::path outputFolder = "output";
+
+std::string genCmd(const std::filesystem::path filePath) {
+        return "magick " + inputFolder.string() + "/'" + filePath.filename().string() + "' " + outputFolder.string() + "/'" + filePath.filename().stem().string() + ".dds'";
+}
+
+
 int main() {
-    // input folder
-    std::filesystem::path inputFolder = "input";
-
-    // output folder
-    std::filesystem::path outputFolder = "output";
-
 
     auto start2 = std::chrono::high_resolution_clock::now(); // CHRONO CLOCK: starts time for general function
 
@@ -17,30 +21,31 @@ int main() {
         if (entry.is_regular_file()) {
             // Get the file path
             std::filesystem::path filePath = entry.path();
-            std::cout << "Found file: " << filePath << std::endl;
+
+            // don't bitch about gitignore file please!!!
+            if (filePath.filename() == ".gitignore") {
+                continue;
+            };
+
+            std::cout << "Found file: " << filePath << "\n";
 
             auto start = std::chrono::high_resolution_clock::now(); // CHRONO CLOCK: starts time
 
-            std::string inputfolderchar = inputFolder.string();
-            std::string filepathchar = filePath.filename().string(); // This works
-            std::string filepathchar2 = filePath.filename().stem().string(); // This works
-            std::string outputfolderchar = outputFolder.string();
-
-            // wtf
-            std::string command = "magick " + inputfolderchar + "/" + filepathchar + " " + outputfolderchar + "/" + filepathchar2 + ".dds";
-
-            // should work
+            std::string command = genCmd(filePath);
             std::system(command.c_str());
 
             auto end = std::chrono::high_resolution_clock::now(); // CHRONO CLOCK: ends time
             std::chrono::duration<double> duration = end - start; // CHRONO CLOCK: calculates the time
 
-            std::cout << "Converted in " << std::setprecision(5) << duration.count() << " seconds." << std::endl;
+            std::cout << "Converted in " << std::setprecision(5) << duration.count() << " seconds." << "\n";
         }
     }
 
     auto end2 = std::chrono::high_resolution_clock::now(); // CHRONO CLOCK: starts time for general function
     std::chrono::duration<double> duration = end2 - start2; // CHRONO CLOCK: calculates time for general function
-    std::cout << "Converted all files in " << std::setprecision(5) << duration.count() << " seconds." << std::endl;
+
+
+
+    std::cout << "Converted all files in " << std::setprecision(5) << duration.count() << " seconds." << "\n";
     return 0;
 }
