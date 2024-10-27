2e945eed16f13da28df2aeffdbcef2e5336ff641
Matthew <61363778+matieo33@users.noreply.github.com>
2024-10-27 14:17:54 +0100

    Update dds.cpp

--- dds.cpp
+++ dds.cpp
@@ -1,21 +1,25 @@
 #include <iostream>
 #include <filesystem>
 #include <chrono>
+#include <iomanip> // Include this header for std::setprecision
 
 std::filesystem::path filePath;
 
 const std::filesystem::path inputFolder = "input";
 const std::filesystem::path outputFolder = "output";
 
-std::string genCmd(const std::filesystem::path filePath) {
-        return "magick " + inputFolder.string() + "/'" + filePath.filename().string() + "' " + outputFolder.string() + "/'" + filePath.filename().stem().string() + ".dds'";
+std::string genCmd(const std::filesystem::path filePath) {    
+    #if __linux__
+    std::string command = "magick " + inputFolder.string() + "/'" + filePath.filename().string() + "' " + outputFolder.string() + "/'" + filePath.filename().stem().string() + ".dds'"
+    #elif _WIN32
+    std::string command = "magick \"" + inputFolder.string() + "\\" + filePath.filename().string() + "\" \"" +
+                            outputFolder.string() + "\\" + filePath.filename().stem().string() + ".dds\"";
+    #endif
+    return command;
 }
 
-
 int main() {
-
     auto start2 = std::chrono::high_resolution_clock::now(); // CHRONO CLOCK: starts time for general function
-
 
     for (const auto& entry : std::filesystem::directory_iterator(inputFolder)) {
         if (entry.is_regular_file()) {
@@ -25,7 +29,7 @@ int main() {
             // don't bitch about gitignore file please!!!
             if (filePath.filename() == ".gitignore") {
                 continue;
-            };
+            }
 
             std::cout << "Found file: " << filePath << "\n";
 
@@ -44,8 +48,10 @@ int main() {
     auto end2 = std::chrono::high_resolution_clock::now(); // CHRONO CLOCK: starts time for general function
     std::chrono::duration<double> duration = end2 - start2; // CHRONO CLOCK: calculates time for general function
 
-
-
     std::cout << "Converted all files in " << std::setprecision(5) << duration.count() << " seconds." << "\n";
+
+    std::cout << "Press Enter to exit...";
+    std::cin.ignore();
+
     return 0;
 }
