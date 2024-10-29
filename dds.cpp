5eceaee0802117b1181f751338fbe5fbceef2502
Matthew <61363778+matieo33@users.noreply.github.com>
2024-10-29 15:32:52 +0100

    Update dds.cpp

--- dds.cpp
+++ dds.cpp
@@ -1,57 +1,87 @@
 #include <iostream>
 #include <filesystem>
 #include <chrono>
-#include <iomanip> // Include this header for std::setprecision
+#include <iomanip> // std::setprecision
+#include <set>
 
 std::filesystem::path filePath;
+bool success;
 
 const std::filesystem::path inputFolder = "input";
 const std::filesystem::path outputFolder = "output";
 
-std::string genCmd(const std::filesystem::path filePath) {    
+std::string genCmd(const std::filesystem::path filePath) {
+    std::string command;    
+    std::string fileExt = filePath.extension().string();
+    const std::set<std::string> supportedExt = {".png", ".jpg", ".jpeg", ".bmp", ".tga", ""};
+
     #if __linux__
-    std::string command = "magick " + inputFolder.string() + "/'" + filePath.filename().string() + "' " + outputFolder.string() + "/'" + filePath.filename().stem().string() + ".dds'";
+    if (supportedExt.find(fileExt) != supportedExt.end()) { // Is supported extension (to dds)
+        command = "magick " + inputFolder.string() + "/'" + filePath.filename().string() + "' " + outputFolder.string() + "/'" + filePath.filename().stem().string() + ".dds'";
+        success = true;
+        return command;
+    }
+    else { // Neither. Error.
+        success = false;
+        return command;
+    }
+
     #elif _WIN32
-    std::string command = "magick \"" + inputFolder.string() + "\\" + filePath.filename().string() + "\" \"" +
+    if (supportedExt.find(fileExt) != supportedExt.end()) { // Is supported extension (to dds)
+        command = "magick \"" + inputFolder.string() + "\\" + filePath.filename().string() + "\" \"" +
                             outputFolder.string() + "\\" + filePath.filename().stem().string() + ".dds\"";
+        success = true;
+        return command;                    
+    }
+    else { // Neither. Error.
+        success = false;
+        return command;
+    }
     #endif
-    return command;
 }
 
-int main() {
+int convert() {
     auto start2 = std::chrono::high_resolution_clock::now(); // CHRONO CLOCK: starts time for general function
+
+    std::cout << "FILE" << std::setw(52) << "STATUS" << std::setw(52) << "TIME\n\n";
 
     for (const auto& entry : std::filesystem::directory_iterator(inputFolder)) {
         if (entry.is_regular_file()) {
             // Get the file path
             std::filesystem::path filePath = entry.path();
 
-            // don't bitch about gitignore file please!!!
+            // Shush when reading .gitignore file
             if (filePath.filename() == ".gitignore") {
                 continue;
             }
 
-            std::cout << "Found file: " << filePath << "\n";
-
             auto start = std::chrono::high_resolution_clock::now(); // CHRONO CLOCK: starts time
 
-            std::string command = genCmd(filePath);
+            std::string command = genCmd(filePath); // see std::string genCmd
             std::system(command.c_str());
 
             auto end = std::chrono::high_resolution_clock::now(); // CHRONO CLOCK: ends time
             std::chrono::duration<double> duration = end - start; // CHRONO CLOCK: calculates the time
 
-            std::cout << "Converted in " << std::setprecision(5) << duration.count() << " seconds." << "\n";
+            std::cout << std::left << std::setw(50) << filePath.filename().string() 
+            << std::setw(50) << (success ? "success" : "failure, wrong file format or corrupted file") 
+            << std::setw(50) << std::setprecision(5) 
+            << duration.count() << "\n";
         }
     }
 
-    auto end2 = std::chrono::high_resolution_clock::now(); // CHRONO CLOCK: starts time for general function
+    auto end2 = std::chrono::high_resolution_clock::now(); // CHRONO CLOCK: ends time for general function
     std::chrono::duration<double> duration = end2 - start2; // CHRONO CLOCK: calculates time for general function
 
-    std::cout << "Converted all files in " << std::setprecision(5) << duration.count() << " seconds." << "\n";
+    std::cout << "\nConverted all files in " << std::setprecision(5) << duration.count() << " seconds." << "\n";
 
     std::cout << "Press Enter to exit...";
     std::cin.ignore();
+
+}
+
+int main() {
+    convert();
 
     return 0;
 }
